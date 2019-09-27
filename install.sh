123b54452b8ea5df5fd0ae0f8f52d51c8688d747
Brandon Bjelland <brandon@atscale.run>
2019-09-26 22:13:59 -0700

    install added

--- install.sh
+++ install.sh
@@ -5,7 +5,6 @@
 if ! [[ "$(terraform version)" == "Terraform v0.12.9" ]] ; then
     wget https://releases.hashicorp.com/terraform/0.12.9/terraform_0.12.9_linux_amd64.zip
     unzip terraform_0.12.9_linux_amd64.zip
-    mv $(which terraform) $(which terraform).bak
-    mv terraform /usr/local/bin/terraform
+    sudo mv terraform /usr/local/bin/terraform
     rm terraform_0.12.9_linux_amd64.zip
 fi
